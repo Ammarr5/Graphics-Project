391ce82d3b5b6c073d2e927cc878ccb78f4fd648
adel ashraf <adel4ashraf@outlook.com>
2022-05-30 17:50:46 +0200

    added ellipse midpoint and polar

--- Graphics Project/EllipseDrawerMidpoint.cpp
+++ Graphics Project/EllipseDrawerMidpoint.cpp
@@ -27,5 +27,37 @@ void EllipseDrawerMidpoint::draw(ShapeData* data)
     glColor3f(r, g, b);
     int RA = (int)sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
     int RB = (int)sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
-    int x = 0;
+    double y = RB, x = 0.0, a2 = RA * RA, b2 = RB * RB;
+    int d = (int)(b2 - a2 * b + 0.25 * a2);
+    Draw4Points(x1, y1, (int)round(x), (int)round(y));
+    while (b2 * x <= a2 * y)
+    {
+        x++;
+        if (d > 0)
+        {
+            y--;
+            d += (int)(b2 * (2 * x + 3) + a2 * (-2 * y + 2));
+        }
+        else
+        {
+            d += (int)(b2 * (2 * x + 3));
+        }
+        Draw4Points(x1, y1, (int)round(x), (int)round(y));
+    }
+    d = (int)((x+0.5) * (x+0.5) * b2 + a2 * (y - 1)*(y-1) - a2 * b2);
+    Draw4Points(x1,y1,(int)round(x),(int)round(y));
+    while(y!=0)
+    {
+        y--;
+        if (d>0)
+        {
+            d+=(int)(a2 * (-2*y+3));
+        }
+        else
+        {
+            x++;
+            d += (int)(b2 *(2*x+2) + a2 * (-2*y+3));
+        }
+        Draw4Points(x1,y1,(int)round(x),(int)round(y));
+    }
 }
--- Graphics Project/EllipseDrawerPolar.cpp
+++ Graphics Project/EllipseDrawerPolar.cpp
@@ -1,5 +1,6 @@
 #include "EllipseDrawerPolar.h"
 #include "EllipseData.h"
+#include <algorithm>
 
 #include <Windows.h>
 #include <gl\GLu.h>
@@ -7,6 +8,7 @@
 
 #pragma comment(lib, "opengl32")
 #pragma comment(lib, "glu32")
+
 
 void EllipseDrawerPolar::draw(ShapeData* data)
 {
@@ -27,5 +29,19 @@ void EllipseDrawerPolar::draw(ShapeData* data)
     glColor3f(r, g, b);
     int RA = (int)sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
     int RB = (int)sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
+    int x = RA;
+    double y = 0;
+    double theta = 0;
+    double dTheta = 1.0 / std::max(RA, RB);
+    Draw4Points(x1, y1, x, (int)y);
+    while (theta <= 45)
+    {
+        x = (int)round(RA * cos(theta));
+        y = round(RB * sin(theta));
+        Draw4Points(x1, y1, x, (int)y);
+        theta += dTheta;
+    }
+
+
 
 }
