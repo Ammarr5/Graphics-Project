f516774c240f2c7dba8146bef81f0542771d96f7
zeyad taher <zeyadtaher16@gmail.com>
2022-05-30 17:58:58 +0200

    line parametric color fixed

--- Graphics Project/LineDrawerParametric.cpp
+++ Graphics Project/LineDrawerParametric.cpp
@@ -15,9 +15,10 @@ void LineDrawerParametric::draw(ShapeData* data) {
     int y1 = lData.y1;
     int x2 = lData.x2;
     int y2 = lData.y2;
+    COLORREF color = lData.color;
 
     glBegin(GL_POINTS);
-    glColor3f(1, 0, 0);
+    glColor3f((float)GetRValue(color)/255, (float)GetGValue(color)/255, (float) GetBValue(color)/255);
     for (double t = 0; t <= 1; t += 0.001) {
         int x = x1 + (x2 - x1) * t;
         int y = y1 + (y2 - y1) * t;
